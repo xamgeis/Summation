64bc92013aa519ebf51e10e759845f97ec549f57
$ <max.t.geislinger@gmail.com>
2016-04-19 17:24:44 -0700

    changed to subtraction.

--- app/src/main/java/com/example/omnipotent/summation/MainActivity.java
+++ app/src/main/java/com/example/omnipotent/summation/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public int sum( int n1, int n2){
-        return n1 + n2;
+        return n1 - n2;
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
